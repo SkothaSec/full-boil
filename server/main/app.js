41d762d9a601b909d88d4c28e3fec3601bc4d990
Christian Gorke <dev.cmgorke@gmail.com>
2019-10-31 18:13:17 -0600

    setupProxy started with http-proxy-middleware

--- server/main/app.js
+++ server/main/app.js
@@ -1,11 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
+var proxy = require('http-proxy-middleware');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes')
-
 var app = express();
 
 app.use(logger('dev'));
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/', indexRouter)
 
